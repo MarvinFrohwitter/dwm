cfcf2e110269dbfea53d8e1a94e5cba9ffc697fe
Marvin Frohwitter <marvin.frohwitter@web.de>
2022-10-09 01:26:54 +0200

    dwm-vanitygaps-6.2.diff

--- config.h
+++ config.h
@@ -3,6 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -78,11 +83,26 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
+#define FORCE_VSPLIT 1  /* nrowgrid layout: force two clients to always split vertically */
+#include "vanitygaps.c"
+
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "[@]",      spiral },
+	{ "[\\]",     dwindle },
+	{ "H[]",      deck },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
+	{ "HHH",      grid },
+	{ "###",      nrowgrid },
+	{ "---",      horizgrid },
+	{ ":::",      gaplessgrid },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ NULL,       NULL },
 };
 
 /* key definitions */
@@ -214,6 +234,24 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
+
+	{ ControlMask,              XK_j,      incrgaps,       {.i = +1 } },
+	{ ControlMask,              XK_k,      incrgaps,       {.i = -1 } },
+	{ ControlMask|ShiftMask,    XK_j,      incrigaps,      {.i = +1 } },
+	{ ControlMask|ShiftMask,    XK_k,      incrigaps,      {.i = -1 } },
+	{ ControlMask,              XK_h,      incrogaps,      {.i = +1 } },
+	{ ControlMask,              XK_l,      incrogaps,      {.i = -1 } },
+	/* { ControlMask,              XK_6,      incrihgaps,     {.i = +1 } }, */
+	/* { ControlMask|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } }, */
+	/* { ControlMask,              XK_7,      incrivgaps,     {.i = +1 } }, */
+	/* { ControlMask|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } }, */
+	/* { ControlMask,              XK_8,      incrohgaps,     {.i = +1 } }, */
+	/* { ControlMask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } }, */
+	/* { ControlMask,              XK_9,      incrovgaps,     {.i = +1 } }, */
+	/* { ControlMask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } }, */
+
+	{ ControlMask,              XK_0,      togglegaps,     {0} },
+	{ ControlMask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_q,      killclient,     {0} },
