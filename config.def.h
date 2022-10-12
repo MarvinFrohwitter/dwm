a0bcafe7a936a6577e0c1f646cd222bec5f798c0
Marvin Frohwitter <marvin.frohwitter@web.de>
2022-10-12 21:19:48 +0200

    bar check and picom testing

--- config.def.h
+++ config.def.h
@@ -3,26 +3,26 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
 static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int sidepad            = 20;       /* horizontal padding of bar */
 
 static const unsigned int stairpx   = 20;       /* depth of the stairs layout */
 static const int stairdirection     = 1;        /* 0: left-aligned, 1: right-aligned */
 static const int stairsamesize      = 1;        /* 1 means shrink all the staired windows to the same size */
 
 static const double activeopacity   = 1.0f;     /* Window opacity when it's focused (0 <= opacity <= 1) */
-static const double inactiveopacity = 0.9f;   /* Window opacity when it's inactive (0 <= opacity <= 1) */
+static const double inactiveopacity = 1.0f;   /* Window opacity when it's inactive (0 <= opacity <= 1) */
 static       Bool bUseOpacity       = True;     /* Starts with opacity on any unfocused windows */
-static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:pixelsize=15:antialias=true:autohint=true", "JoyPixels:size=12:antialias=true:autohint=true" };
 /* static const char *fonts2[]          = { "NotoColorEmoji:pixelsize=15:antialias=true:autohint=true" }; */
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:pixelsize=12:antialias=true:autohint=true";
@@ -84,7 +84,7 @@ static const Rule rules[] = {
 	{ "firefox",     NULL,        NULL,        1 << 7,       0,     0,           0,      -1 },
 	{ "Alacritty",   NULL,        "newsboat",  1 << 8,       0,     0,           0,      -1 },
 	{ "Alacritty",   NULL,        "notetaker", 0,            1,     0,           0,      -1 },
-	{ "Alacritty",   NULL,        NULL,        0,            1,     1,           0,      -1 },
+	{ "Alacritty",   NULL,        NULL,        0,            1,     0,           0,      -1 },
 	{ "St",          NULL,        NULL,        0,            0,     1,           0,      -1 },
 	{ "gimp",        NULL,        NULL,        0,            0,     0,           0,      -1 },
 	{ NULL,          NULL,     "Event Tester", 0,            0,     0,           1,      -1 }, /* xev */
