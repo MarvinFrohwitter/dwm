56801edcb0a26de86fde7f327899c24c0a50551e
Marvin Frohwitter <marvin.frohwitter@web.de>
2024-05-26 15:13:51 +0200

    Added layoutmenu.sh

--- layoutmenu.sh
+++ layoutmenu.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-cat <<EOF | xmenu
-[]= Tiled Layout	0
-><> Floating Layout	1
-[M] Monocle Layout	2
+cat  <<EOF | xmenu
+[]=  tile	0
+[M]  monocle	1
+[@]  spiral	2
+[\\] dwindle	3
+H[]  deck	4
+TTT  bstack	5
+===  bstackhoriz	6
+HHH  grid	7
+###  nrowgrid	8
+---  horizgrid	9
+:::  gaplessgrid	10
+|||  col	11
+[S]  stairs	12
+|M|  centeredmaster	13
+>M>  centeredfloatingmaster	14
+III  horizontal 15
+><>  Floating Layout	16
 EOF
